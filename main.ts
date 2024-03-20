10aa96fbdc70fa9306cc6c2e72ce914e0cd94c5c
areesha-22 <110978549+areesha-22@users.noreply.github.com>
2024-03-21 02:31:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+// lower case
+console.log("lowercase:", person_name.toLowerCase())
+ 
+//uppercase
+console.log("UPPERCASE:",person_name.toUpperCase())
+
+//titelcase
+console.log("titelcase:",person_name.replace(/\bw/g,c => c.toUpperCase()))
